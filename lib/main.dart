b562e5f40bd693a610520be16e9a87876e23c24c
PrerakModia <88586985+PrerakModia@users.noreply.github.com>
2023-03-06 22:32:07 +0530

    THEME

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:pccmobile/screens/dashboard.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          textTheme:
+              GoogleFonts.sourceSansProTextTheme(Theme.of(context).textTheme)),
+      debugShowCheckedModeBanner: false,
+      home: const Dashboard(),
+    );
+  }
+}
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  var index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/1.jpg'), fit: BoxFit.cover)),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 68, 80, 88),
+        selectedItemColor: Colors.yellow,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.warning), label: "ALERTS"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.graphic_eq_outlined), label: "WIRES"),
+        ],
+        currentIndex: index,
+        onTap: (int i) {
+          setState(() {
+            index = i;
+          });
+        },
+      ),
+    );
+  }
+}
