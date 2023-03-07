78be8be2d6fcb0714f66ed94a7456b6e286cf9e3
PrerakModia <88586985+PrerakModia@users.noreply.github.com>
2023-03-07 11:01:15 +0530

    HomePage Screen

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -13,9 +13,120 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(10),
+        height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/1.jpg'), fit: BoxFit.cover)),
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 237, 61, 216),
+                        borderRadius: BorderRadius.circular(7)),
+                    height: MediaQuery.of(context).size.height * 0.13,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: const GridTile(
+                      header: Text(
+                        'Current Fed Balance',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '\$ 74.5 B',
+                          style: TextStyle(fontSize: 44, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 75, 185, 236),
+                        borderRadius: BorderRadius.circular(7)),
+                    height: MediaQuery.of(context).size.height * 0.13,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: const GridTile(
+                      header: Text(
+                        'Current CHIPS Balance',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '\$ 6.34 B',
+                          style: TextStyle(fontSize: 44, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.01,
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 75, 185, 236),
+                        borderRadius: BorderRadius.circular(7)),
+                    height: MediaQuery.of(context).size.height * 0.13,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: const GridTile(
+                      header: Text(
+                        'Open Fed Balance',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '\$ 71.5 B',
+                          style: TextStyle(fontSize: 44, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 75, 185, 236),
+                        borderRadius: BorderRadius.circular(7)),
+                    height: MediaQuery.of(context).size.height * 0.13,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: const GridTile(
+                      header: Text(
+                        'Net Debit Cap',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '\$ 69.1 B',
+                          style: TextStyle(fontSize: 44, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Color.fromARGB(255, 68, 80, 88),
